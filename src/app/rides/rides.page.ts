555b944826889768ac0049819ebc633a227d4595
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-09-30 16:56:56 +0300

    Init 2

---
+++ src/app/rides/rides.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rides',
+  templateUrl: './rides.page.html',
+  styleUrls: ['./rides.page.scss'],
+})
+export class RidesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
